4ccc82a503c1e70cc8430b94683a1521de12c48a
Themis <idonotwantspams@gmail.com>
2023-10-12 02:10:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ Hooks.on('preUpdateToken', (tdoc, changes, options, userId) => {
   const sourcePos = {x: tdoc.x, y: tdoc.y};
   const targetPos = {x: changes.x, y: changes.y};
   if (!targetPos.x) targetPos.x = sourcePos.x;
-  if (!targetPos.x) targetPos.y = sourcePos.y;
+  if (!targetPos.y) targetPos.y = sourcePos.y;
   const ray = new Ray(sourcePos, targetPos);
   if (CONFIG.Canvas.polygonBackends.move.testCollision(ray.A,ray.B,{mode:'any',type: 'move'})) options.animate = false;
 });
