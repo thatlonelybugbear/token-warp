2212cfeaffb6d18d5e29ef3b3fa106f355f5929d
Themis <idonotwantspams@gmail.com>
2023-10-15 05:39:35 +0100

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,7 +1,5 @@
 'use strict';
 
-import Constants from './src/constants.js';
-import Settings from './src/settings.js';
 import { _preUpdateToken, _wallsBlockMovement } from './src/tokenwarp.js';
 
 Hooks.once('init', () => {
