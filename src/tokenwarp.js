66f40fd136bb0c97aeba3bc5989d5b6312741b3a
Themis <idonotwantspams@gmail.com>
2023-10-17 12:34:02 +0100

    tokenwarp.js - code cleanup

--- src/tokenwarp.js
+++ src/tokenwarp.js
@@ -4,48 +4,47 @@ const settings = new Settings();
 
 /*  Functions */
 export function _preUpdateToken(tdoc, changes, options, userId) {
-  if (!game.users.get(userId).isGM && settings.movementSpeed) {
-    return foundry.utils.setProperty(
-      options,
-      'animation.movementSpeed',
-      settings.movementSpeed
-    );
-  }
-  if (
-    !game.users.get(userId).isGM ||
-    (!changes.x && !changes.y) ||
-    options.animate === false
-  )
-    return;
-  if (settings.movementSwitch && !settings.excludedScene) {
-    options.animate = false;
-    return true;
-  }
-  if (_wallsBlockMovement(tdoc, changes)) {
-    options.animate = false;
-    return true;
-  }
-  if (settings.movementSpeed)
-    foundry.utils.setProperty(
-      options,
-      'animation.movementSpeed',
-      settings.movementSpeed
-    );
-  if (settings.excludedScene && !_wallsBlockMovement(tdoc, changes)) return true;
+	if (
+		(changes.x == undefined && changes.y == undefined) ||
+		options.animate == false
+	)
+		return;
+
+	if (
+		settings.movementSpeed != 6 &&
+		options.animation?.movementSpeed != settings.movementSpeed
+	) {
+		foundry.utils.setProperty(
+			options,
+			'animation.movementSpeed',
+			settings.movementSpeed
+		);
+	}
+
+	if (!game.users.get(userId).isGM) return true;
+
+	if (settings.movementSwitch && !settings.excludedScene) {
+		options.animate = false;
+		return true;
+	}
+	if (_wallsBlockMovement(tdoc, changes)) {
+		options.animate = false;
+		return true;
+	}
 }
 
 export function _wallsBlockMovement(tdoc, changes) {
-  if (!settings.wallBlock) return false;
-  const sourceCenter = tdoc.object.center;
-  const targetPos = { x: (changes.x ??= tdoc.x), y: (changes.y ??= tdoc.y) };
-  const offset = { x: sourceCenter.x - tdoc.x, y: sourceCenter.y - tdoc.y };
-  const targetCenter = { x: targetPos.x + offset.x, y: targetPos.y + offset.y };
-  const ray = new Ray(sourceCenter, targetCenter);
-  if (
-    CONFIG.Canvas.polygonBackends.move.testCollision(ray.A, ray.B, {
-      mode: 'any',
-      type: 'move',
-    })
-  )
-    return true;
+	if (!settings.wallBlock) return false;
+	const sourceCenter = tdoc.object.center;
+	const targetPos = { x: (changes.x ??= tdoc.x), y: (changes.y ??= tdoc.y) };
+	const offset = { x: sourceCenter.x - tdoc.x, y: sourceCenter.y - tdoc.y };
+	const targetCenter = { x: targetPos.x + offset.x, y: targetPos.y + offset.y };
+	const ray = new Ray(sourceCenter, targetCenter);
+	if (
+		CONFIG.Canvas.polygonBackends.move.testCollision(ray.A, ray.B, {
+			mode: 'any',
+			type: 'move',
+		})
+	)
+		return true;
 }
