fd2e411420c5ab3a64b29280b1cfd3fcb18f74f7
Themis <idonotwantspams@gmail.com>
2024-05-17 22:38:27 +0100

    Update tokenwarp.js
    - Adding more compatibility with Elevation Ruler.
    - Adding a default Token Warp KeyQ to teleport tokens when pressed. 
    - Respecting the boundaries of the padding when moving by arrows or by dragging the tokens, except for the case that KeyQ is pressed, or Elevation Ruler's toggle pathfinding button.

--- src/tokenwarp.js
+++ src/tokenwarp.js
@@ -5,6 +5,59 @@ const settings = new Settings();
 /*  Functions */
 export function _preUpdateToken(tdoc, changes, options, userId) {
 	if ((changes.x || changes.y) && options.animate !== false) {
+		const ev = event;
+		const keyTW =
+			/*game.keybindings.get('tokenwarp', 'teleport')?.[0].key || */ 'KeyQ';
+		const keyER = game.modules.get('elevationruler')?.active
+			? game.keybindings.get('elevationruler', 'togglePathfinding')[0].key
+			: keyTW;
+		const hasKey =
+			ev?.view?.game.keyboard.downKeys.has(keyER) ||
+			ev?.view?.game.keyboard.downKeys.has(keyTW);
+		const ruler = canvas.controls.ruler;
+		const { segments } = ruler;
+		const { size, distance } = canvas.scene.grid || {};
+		const finalSegment = segments?.at(-1);
+		const destination = finalSegment
+			? { x: finalSegment.ray.B.x, y: finalSegment.ray.B.y }
+			: { x: changes.x ?? tdoc.x, y: changes.y ?? tdoc.y };
+		const isRulerMoving = ruler._state === Ruler.STATES.MOVING;
+		if (
+			isRulerMoving &&
+			(hasKey ||
+				(!settings.excludedScene &&
+					(settings.movementSwitch ||
+						(game.users.get(userId).isGM &&
+							settings.wallBlock &&
+							_wallsBlockMovement(tdoc, segments)) ||
+						canvas.scene.dimensions.sceneRect.x > destination.x ||
+						canvas.scene.dimensions.sceneRect.x +
+							canvas.scene.dimensions.sceneRect.width -
+							canvas.grid.size * tdoc.width <
+							destination.x ||
+						canvas.scene.dimensions.sceneRect.y > destination.y ||
+						canvas.scene.dimensions.sceneRect.y +
+							canvas.scene.dimensions.sceneRect.height -
+							canvas.grid.size * tdoc.height <
+							destination.y)))
+		) {
+			let elevation;
+			let update = {};
+			if (segments?.length) {
+				update = getAdjustedDestination(tdoc, segments, hasKey);
+				elevation = segments.at(-1).waypointElevationIncrement
+					? tdoc.elevation +
+					  Math.round(
+							(segments.at(-1).waypointElevationIncrement * distance) / size
+					  )
+					: tdoc.elevation;
+				update.elevation = elevation;
+			} else {
+				update = getAdjustedDestination(tdoc, segments ?? destination, hasKey);
+			}
+			tdoc.update(update, { animate: false, animation: {} });
+			return false;
+		}
 		if (settings.movementSpeed)
 			foundry.utils.setProperty(
 				options,
@@ -12,31 +65,77 @@ export function _preUpdateToken(tdoc, changes, options, userId) {
 				settings.movementSpeed
 			);
 		if (
-			options.rulerSegment || //force Elevation ruler compatibility. If that parameter exists, let ER handle it.
-			settings.excludedScene
-		)
-			return true;
-		if (
-			settings.movementSwitch ||
-			(game.users.get(userId).isGM && _wallsBlockMovement(tdoc, changes))
-		)
-			options.animate = false;
+			canvas.scene.dimensions.sceneRect.x > destination.x ||
+			canvas.scene.dimensions.sceneRect.x +
+				canvas.scene.dimensions.sceneRect.width -
+				canvas.grid.size * tdoc.width <
+				destination.x ||
+			canvas.scene.dimensions.sceneRect.y > destination.y ||
+			canvas.scene.dimensions.sceneRect.y +
+				canvas.scene.dimensions.sceneRect.height -
+				canvas.grid.size * tdoc.height <
+				destination.y
+		) {
+			changes.x = Math.clamped(
+				destination.x,
+				canvas.scene.dimensions.sceneRect.x,
+				canvas.scene.dimensions.sceneRect.x +
+					canvas.scene.dimensions.sceneRect.width -
+					canvas.grid.size * tdoc.width
+			);
+			changes.y = Math.clamped(
+				destination.y,
+				canvas.scene.dimensions.sceneRect.y,
+				canvas.scene.dimensions.sceneRect.y +
+					canvas.scene.dimensions.sceneRect.height -
+					canvas.grid.size * tdoc.height
+			);
+		}
+		return true;
 	}
-	return true;
 }
 
-export function _wallsBlockMovement(tdoc, changes) {
-	if (!settings.wallBlock) return false;
-	const sourceCenter = tdoc.object.center;
-	const targetPos = { x: (changes.x ??= tdoc.x), y: (changes.y ??= tdoc.y) };
-	const offset = { x: sourceCenter.x - tdoc.x, y: sourceCenter.y - tdoc.y };
-	const targetCenter = { x: targetPos.x + offset.x, y: targetPos.y + offset.y };
-	const ray = new Ray(sourceCenter, targetCenter);
-	if (
-		CONFIG.Canvas.polygonBackends.move.testCollision(ray.A, ray.B, {
-			mode: 'any',
-			type: 'move',
-		})
-	)
-		return true;
+function _wallsBlockMovement(tdoc, segments) {
+	if (!segments?.length) return false;
+	for (const segment of segments) {
+		if (tdoc.object.checkCollision(segment.ray.B, { origin: segment.ray.A })) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function getAdjustedDestination(tdoc, segments, hasKey) {
+	const origin = segments.length ? segments[0].ray.A : { x: tdoc.x, y: tdoc.y };
+	const destination = segments.length ? segments.at(-1).ray.B : segments;
+	const s2 =
+		canvas.scene.grid.type === CONST.GRID_TYPES.GRIDLESS
+			? 1
+			: canvas.dimensions.size / 2;
+	const dx = Math.round((tdoc.x - origin.x) / s2) * s2;
+	const dy = Math.round((tdoc.y - origin.y) / s2) * s2;
+	const r = new Ray(origin, destination);
+	const adjustedDestination = canvas.grid.grid._getRulerDestination(
+		r,
+		{ x: dx, y: dy },
+		tdoc.object
+	);
+	if (hasKey) return adjustedDestination;
+	else
+		return {
+			x: Math.clamped(
+				adjustedDestination.x,
+				canvas.scene.dimensions.sceneRect.x,
+				canvas.scene.dimensions.sceneRect.x +
+					canvas.scene.dimensions.sceneRect.width -
+					canvas.grid.size * tdoc.width
+			),
+			y: Math.clamped(
+				adjustedDestination.y,
+				canvas.scene.dimensions.sceneRect.y,
+				canvas.scene.dimensions.sceneRect.y +
+					canvas.scene.dimensions.sceneRect.height -
+					canvas.grid.size * tdoc.height
+			),
+		};
 }
