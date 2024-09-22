eaece4563ed0a8a18e12cc8b686519267dbe78be
Themis <idonotwantspams@gmail.com>
2024-09-22 23:29:23 +0100

    17 teleport out of bounds should be disallowed for player clients (#18)
    * Update en.json
    
    * Update tokenwarp.js

--- src/tokenwarp.js
+++ src/tokenwarp.js
@@ -6,74 +6,75 @@ const name = Constants.MODULE_NAME;
 
 /*  Functions */
 export function _preUpdateToken(tdoc, changes, options, userId) {
-	if (
-		(!changes.x && !changes.y) ||
-		(changes.x == tdoc.x && changes.y == tdoc.y) ||
-		options.animate == false ||
-		options.teleport == true ||
-		options.tokenwarped == true
-	)
-		return;
-	const token = tdoc.object;
-	const ev = event;
-	const keyTW = settings.teleportKey;
-	const keyER = getElevationRulerKey();
-	const hasKey =
-		isKeyPressed(ev, keyTW) || elevationRulerPathfindingKeybind(ev, keyER);
-	const {
-		excludedScene,
-		movementSpeed,
-		movementSwitch,
-		outOfBounds,
-		debug,
-		migration,
-	} = settings;
-	if (debug)
-		console.warn(`${name} settings: ||`, {
-			excludedScene,
-			movementSpeed,
-			movementSwitch,
-			outOfBounds,
-			debug,
-			migration,
-		});
-	const destination = { x: changes.x, y: changes.y }; //topLeft
-	const origin = { x: tdoc.x, y: tdoc.y }; //topLeft
-	const destinationCenter = token.getCenterPoint(destination);
+  if (
+    (!changes.x && !changes.y) ||
+    (changes.x == tdoc.x && changes.y == tdoc.y) ||
+    options.animate == false ||
+    options.teleport == true ||
+    options.tokenwarped == true
+  )
+    return;
+  const isGM = game.users.get(userId).isGM;
+  const token = tdoc.object;
+  const ev = event;
+  const keyTW = settings.teleportKey;
+  const keyER = getElevationRulerKey();
+  const hasKey =
+    isKeyPressed(ev, keyTW) || elevationRulerPathfindingKeybind(ev, keyER);
+  const {
+    excludedScene,
+    movementSpeed,
+    movementSwitch,
+    outOfBounds,
+    debug,
+    migration,
+  } = settings;
+  if (debug)
+    console.warn(`${name} settings: ||`, {
+      excludedScene,
+      movementSpeed,
+      movementSwitch,
+      outOfBounds,
+      debug,
+      migration,
+    });
+  const destination = { x: changes.x, y: changes.y }; //topLeft
+  const origin = { x: tdoc.x, y: tdoc.y }; //topLeft
+  const destinationCenter = token.getCenterPoint(destination);
 
-	const originCenter = token.center;
+  const originCenter = token.center;
 
-	const ruler = canvas.controls.ruler;
-	const { segments } = ruler || {};
-	const finalSegment = segments.find((s) => s.last);
-	const activeRulerModules = getActiveRulers();
-	if (activeRulerModules == 'both') return true;
-	const tokenCenterPointDiff = token.getCenterPoint({ x: 0, y: 0, z: 0 });
-	const finalDestination =
-		activeRulerModules == 'ER' && segments.length > 1
-			? {
-				x: finalSegment.ray.B.x - tokenCenterPointDiff.x,
-				y: finalSegment.ray.B.y - tokenCenterPointDiff.y,
-				z: finalSegment.ray.B.z,
-			}
-			: activeRulerModules == 'DR' && segments.length
-				? token.getSnappedPosition({
-					x: finalSegment.ray.B.x - tokenCenterPointDiff.x,
-					y: finalSegment.ray.B.y - tokenCenterPointDiff.y,
-					z: finalSegment.ray.B.z,
-				})
-				: destination;
-  	if (excludedScene || hasKey) {
-	  if (!hasKey) return getMovementSpeed(options, settings);
+  const ruler = canvas.controls.ruler;
+  const { segments } = ruler || {};
+  const finalSegment = segments.find((s) => s.last);
+  const activeRulerModules = getActiveRulers();
+  if (activeRulerModules == 'both') return true;
+  const tokenCenterPointDiff = token.getCenterPoint({ x: 0, y: 0, z: 0 });
+  const finalDestination =
+    activeRulerModules == 'ER' && segments.length > 1
+      ? {
+          x: finalSegment.ray.B.x - tokenCenterPointDiff.x,
+          y: finalSegment.ray.B.y - tokenCenterPointDiff.y,
+          z: finalSegment.ray.B.z,
+        }
+      : activeRulerModules == 'DR' && segments.length
+      ? token.getSnappedPosition({
+          x: finalSegment.ray.B.x - tokenCenterPointDiff.x,
+          y: finalSegment.ray.B.y - tokenCenterPointDiff.y,
+          z: finalSegment.ray.B.z,
+        })
+      : destination;
+  if (excludedScene || (hasKey && isGM)) {
+    if (!hasKey) return getMovementSpeed(options, settings);
     else {
-		  tdoc.update(finalDestination, {
-			  animate: false,
-			  teleport: true,
-			  tokenwarped: true,
-		  });
-		  return false;
-	  };
-	};
+      tdoc.update(finalDestination, {
+        animate: false,
+        teleport: true,
+        tokenwarped: true,
+      });
+      return false;
+    }
+  }
 
   if (
     (changes.x !== tdoc.x || changes.y !== tdoc.y) &&
@@ -87,30 +88,30 @@ export function _preUpdateToken(tdoc, changes, options, userId) {
         settings.movementSwitch == 'wallsblock' &&
         token.checkCollision(destinationCenter, { origin: originCenter }))
     ) {
-		options.animate = false;
-		options.teleport = true;
-		options.tokenwarped = true;
+      options.animate = false;
+      options.teleport = true;
+      options.tokenwarped = true;
     }
     if (
       settings.outOfBounds &&
-      !hasKey &&
+      ((isGM && !hasKey) || !isGM) &&
       positionOutOfBounds({ destination, origin, tdoc }) == true
     ) {
-		options.tokenwarped = true;
-		foundry.utils.mergeObject(
-			changes,
-			clampDestinationToSceneRect({ tdoc, destination })
-		);
+      options.tokenwarped = true;
+      foundry.utils.mergeObject(
+        changes,
+        clampDestinationToSceneRect({ tdoc, destination })
+      );
     }
     if (
-      hasKey ||
+      (isGM && hasKey) ||
       positionOutOfBounds({ destination, origin, tdoc }) == 'both'
     ) {
-		options.animate = false;
-		options.teleport = true;
-		options.tokenwarped = true;
+      options.animate = false;
+      options.teleport = true;
+      options.tokenwarped = true;
     }
-	  return getMovementSpeed(options, settings);
+    return getMovementSpeed(options, settings);
   }
 }
 
@@ -196,9 +197,7 @@ export function getActiveRulers() {
   const ER = game.modules.get('elevationruler')?.active;
   const DR = game.modules.get('drag-ruler')?.active;
   if (ER && DR) {
-    ui.notifications.error(
-      game.i18n.localize('TOKENWARP.WarnMultipleRulersActive')
-    );
+    ui.notifications.error(game.i18n.localize("TOKENWARP.WarnMultipleRulersActive"));
     return 'both';
   }
   return ER ? 'ER' : DR ? 'DR' : false;
