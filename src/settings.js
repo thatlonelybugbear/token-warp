fa8c3d6f44e6c3a1ef2ad582cd988cd141a71bcf
thatlonelybugbear <7237090+thatlonelybugbear@users.noreply.github.com>
2026-07-12 04:22:12 +0100

    chore: require Foundry v14

--- src/settings.js
+++ src/settings.js
@@ -3,14 +3,11 @@ import Constants from './constants.js';
 export default class Settings {
 	// KEYS FOR WORLD CONFIG SETTINGS
 	static TOKEN_ANIMATION_SWITCH = 'defaultTokenAnimationSwitch';
-	static WALLS_CANCEL_TOKEN_ANIMATION_GM = 'wallsCancelTokenAnimationGM';
-	static DEFAULT_TOKEN_ANIMATION_SWITCH_OLD = 'defaultTokenAnimationSwitchOLD';
 	static EXCLUDED_SCENES = 'excludedScenes';
 	static DEFAULT_TOKEN_ANIMATION_SPEED = 'defaultTokenAnimationSpeed';
 	static DEFAULT_OUT_OF_BOUNDS_BEHAVIOUR = 'outOfBoundsCheck';
 	static TELEPORT_KEYBIND = 'teleportKey';
 	static DISABLE_ROTATION_KEYBIND = 'disableRotationKey';
-	static MIGRATIONS = 'migration';
 	static DEBUG = 'debug';
 
 	registerSettings() {
@@ -47,32 +44,6 @@ export default class Settings {
 
 		game.settings.register(
 			Constants.MODULE_ID,
-			Settings.DEFAULT_TOKEN_ANIMATION_SWITCH_OLD,
-			{
-				name: 'TOKENWARP.DefaultTokenAnimationSwitchName',
-				hint: 'TOKENWARP.DefaultTokenAnimationSwitchHint',
-				scope: 'world',
-				config: false,
-				default: false,
-				type: Boolean,
-			},
-		);
-
-		game.settings.register(
-			Constants.MODULE_ID,
-			Settings.WALLS_CANCEL_TOKEN_ANIMATION_GM,
-			{
-				name: 'TOKENWARP.WallsCancelTokenAnimationName',
-				hint: 'TOKENWARP.WallsCancelTokenAnimationHint',
-				scope: 'world',
-				config: false,
-				default: false,
-				type: Boolean,
-			},
-		);
-
-		game.settings.register(
-			Constants.MODULE_ID,
 			Settings.DEFAULT_TOKEN_ANIMATION_SPEED,
 			{
 				name: 'TOKENWARP.DefaultTokenAnimationSpeedName',
@@ -101,13 +72,6 @@ export default class Settings {
 				type: Boolean,
 			},
 		);
-
-		game.settings.register(Constants.MODULE_ID, Settings.MIGRATIONS, {
-			name: 'TOKENWARP.Migration',
-			scope: 'world',
-			config: false,
-			type: String,
-		});
 
 		game.settings.register(Constants.MODULE_ID, Settings.DEBUG, {
 			name: 'TOKENWARP.Debug',
@@ -148,13 +112,6 @@ export default class Settings {
 		)[0]?.key;
 	}
 
-	get movementSwitchOLD() {
-		return game.settings.get(
-			Constants.MODULE_ID,
-			Settings.DEFAULT_TOKEN_ANIMATION_SWITCH_OLD,
-		);
-	}
-
 	get movementSwitch() {
 		return game.settings.get(
 			Constants.MODULE_ID,
@@ -186,13 +143,6 @@ export default class Settings {
 		else return false;
 	}
 
-	get wallBlock() {
-		return game.settings.get(
-			Constants.MODULE_ID,
-			Settings.WALLS_CANCEL_TOKEN_ANIMATION_GM,
-		);
-	}
-
 	get movementSpeed() {
 		return game.settings.get(
 			Constants.MODULE_ID,
@@ -205,10 +155,6 @@ export default class Settings {
 			Constants.MODULE_ID,
 			Settings.DEFAULT_OUT_OF_BOUNDS_BEHAVIOUR,
 		);
-	}
-
-	get migration() {
-		return game.settings.get(Constants.MODULE_ID, Settings.MIGRATIONS);
 	}
 
 	get debug() {
